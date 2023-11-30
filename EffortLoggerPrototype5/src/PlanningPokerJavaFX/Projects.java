ca8b5d1ac5d6142ff6ce93e1e228e28fedbe662f
Bmayala10 <149704791+Bmayala10@users.noreply.github.com>
2023-11-30 14:52:28 -0700

    Add files via upload

---
+++ EffortLoggerPrototype5/src/PlanningPokerJavaFX/Projects.java
@@ -0,0 +1,59 @@
+package PlanningPokerJavaFX;
+
+
+public class Projects {
+	
+	Integer weights;
+    String projects;
+    String descriptions;
+    String keywords;
+	
+
+
+	public Projects(Integer weights, String projects, String descriptions, String keywords) {
+		this.weights = weights;
+		this.projects = projects;
+		this.descriptions = descriptions;
+		this.keywords = keywords;
+	}
+
+
+
+	public Integer getWeights() {
+		return weights;
+	}
+	public void setWeights(Integer weights) {
+		this.weights = weights;
+	}
+
+	public String getProjects() {
+		return projects;
+	}
+	public void setProjects(String projects) {
+		this.projects = projects;
+	}
+
+	public String getDescriptions() {
+		return descriptions;
+	}
+	public void setDescriptions(String descriptions) {
+		this.descriptions = descriptions;
+	}
+
+	public String getKeywords() {
+		return keywords;
+	}
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+	
+	@Override
+    public String toString() {
+        return 	"	Backlog item:  " + projects + ";		" +
+                "	Description:  " + descriptions + ";		" +
+                "	Keywords:  " + keywords + ";		" +
+                "	Weight:  " + weights;
+    }
+	
+	
+}
